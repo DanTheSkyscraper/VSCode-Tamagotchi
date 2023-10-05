3b36a837fe13d955d7021f2fafe93899735a6119
Daniel Taherkhanian <daniel.taherkhanian@elev.ga.ntig.se>
2023-10-05 14:33:08 +0200

    V 0.2 Added class, methods, boleans, etc

--- TE21A Daniel Tamagotchi/Program.cs
+++ TE21A Daniel Tamagotchi/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿using TE21A_Daniel_Tamagotchi;
+
+Tamagotchi tama = new();
+
+tama.PrintStats();
+
+
+Console.ReadLine();
---
+++ TE21A Daniel Tamagotchi/Tamagotchi.cs
@@ -0,0 +1,59 @@
+﻿namespace TE21A_Daniel_Tamagotchi;
+
+public class Tamagotchi
+{
+    private int hunger = 0;
+
+    private int boredom = 0;
+
+    private List<string> words;
+    
+    private Random Generator = new Random();
+    
+    public string name;
+
+    private bool isAlive = true;
+
+
+    //----------
+
+    public void Feed()
+    {
+
+    }
+
+    public void Hi()
+    {
+
+    }
+
+    public void Teach()
+    {
+        
+    }
+
+    public void Tick()
+    {
+
+    }
+
+    public void PrintStats()
+    {
+        Console.WriteLine($"Tamagotchi status: {isAlive}");
+        
+        Console.WriteLine($"\nHunger: {hunger}");
+        Console.WriteLine($"Boredom: {boredom}");
+    }
+
+    public bool GetAlive()
+    {
+        return true;
+    }
+
+    private void ReduceBoredom()
+    {
+
+    }
+
+
+}
