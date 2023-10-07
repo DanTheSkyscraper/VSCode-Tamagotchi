2a413e5bbc925c4f4f9fbcfde624724ea3cc9eac
Daniel Taherkhanian <daniel.taherkhanian@elev.ga.ntig.se>
2023-10-07 19:04:02 +0200

    V 0.3 Added more code and alternatives

--- TE21A Daniel Tamagotchi/Program.cs
+++ TE21A Daniel Tamagotchi/Program.cs
@@ -1,8 +1,28 @@
-﻿using TE21A_Daniel_Tamagotchi;
+﻿using System.Security;
 
-Tamagotchi tama = new();
+Console.WriteLine("Hello friend! Welcome to Tamagotchi! Here, you are tasked with taking care of, you guessed it, your very own Tamagotchi!");
 
-tama.PrintStats();
+TamagotchiC tama = new();
+TamagotchiC player = new();
+
+Console.WriteLine("Before we begin, what is your name?");
+player.playerName = Console.ReadLine();
+
+Console.WriteLine("Now, before you start taking care of your new charge, you need to decide what to name them. Pick something you think fits them.");
+tama.tamagotchiName = Console.ReadLine();
+
+Console.WriteLine($"Aww, look at your little buddy! They seem to like it! Take good care of 'em, you hear?");
+
+while(tama.GetAlive() == true)
+{
+    tama.PrintStats();
+
+    Console.WriteLine("Choose your next action...");
+    Console.WriteLine($"Feed {tama.tamagotchiName}");
+
+}
+
+
 
 
 Console.ReadLine();
---
+++ TE21A Daniel Tamagotchi/TamagotchiC.cs
@@ -0,0 +1,92 @@
+﻿using System.Net;
+
+public class TamagotchiC
+{
+    private int tamagotchiHunger = 0;
+
+    private int tamagotchiBoredom = 0;
+
+    private int tamagotchiMood = 50;
+
+    private List<string> words;
+    
+    private Random Generator;
+    
+    public string tamagotchiName;
+
+    public string playerName;
+
+    private bool isAlive = true;
+
+
+    //----------
+
+    public void Feed()
+    {
+        Console.WriteLine("Your Tamagotchi eats the food you gave them. They are not as hungry as before.");
+        tamagotchiHunger -= 2;
+    }
+
+    public void Play()
+    {
+        Console.WriteLine("You play with your Tamagotchi. Their mood has improved!");
+        tamagotchiMood += 2;
+    }
+
+    public void Hi()
+    {
+
+    }
+
+    public void Teach()
+    {
+        
+    }
+
+    public void Tick()
+    {
+        tamagotchiHunger++;
+        tamagotchiBoredom++;
+        tamagotchiMood--;
+        if(tamagotchiHunger > 10 & tamagotchiBoredom > 10)
+        {
+            isAlive = false;
+            Console.WriteLine($"Your Tamagotchi {tamagotchiName} has died! How could you let this happen?!");
+        }
+
+        if (tamagotchiMood > 25) //IS THIS CORRECT?? SHOULD IT BE PLACED ELSEWHERE?
+        {
+            Console.WriteLine($"Your Tamagotchi {tamagotchiName} is upset. Turn that frown upside down!");
+        }
+        else
+        {
+            Console.WriteLine($"Your Tamagotchi {tamagotchiName} is happy. Look at that ball of sunshine!");
+        }
+    }
+
+    public void PrintStats()
+    {
+        Console.WriteLine($"Tamagotchi '{tamagotchiName}' status: {isAlive}");
+        
+
+        Console.WriteLine($"\nHunger: {tamagotchiHunger}");
+        Console.WriteLine($"Boredom: {tamagotchiBoredom}");
+        Console.WriteLine($"Mood: {tamagotchiMood}");
+
+        
+    
+    }
+
+    public bool GetAlive()
+    {
+        return true;
+    }
+
+    private void ReduceBoredom()
+    {
+        Console.WriteLine($"Your Tamagotchi '{tamagotchiName}' is not as bored anymore!");
+        tamagotchiBoredom -= 2;
+    }
+
+
+}
